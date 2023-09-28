3b9f31995c2f21468b7f498ca2a1181eccbd352e
eiren7 <eiren7@live.com>
2023-09-28 20:05:45 +0900

    dbname modified

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   password: process.env.PASSWORD,
   host: process.env.HOST,
   port: process.env.DBPORT,
-  database: "todoapp",
+  database: "to_do_app_db_tdhp",
 });
 
 module.exports = pool;
